ff4b8a0af8442b16e398a1de220a3e5b263c0262
Jakub Plesnik <jakub.plesnik@sde.cz>
2020-02-13 10:42:52 +0100

    Add basic examples to lecture 1

--- Lecture1/BasicConsoleApplication/BasicConsoleApplication/BasicConsoleApplication.cpp
+++ Lecture1/BasicConsoleApplication/BasicConsoleApplication/BasicConsoleApplication.cpp
@@ -1,17 +1,53 @@
 // BasicConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/// Sources are based on codes from http://www.cplusplus.com/doc/tutorial
 
 #include <iostream>
 
-int Sum(int a, int b)
+
+int addition(int a, int b)
 {
 	return a + b;
 }
 
+int subtraction(int a, int b)
+{
+	int r;
+	r = a - b;
+	return r;
+}
+
+void duplicate(int& a, int& b)
+{
+	a *= 2;
+	b *= 2;
+}
 
 int main()
 {
     std::cout << "Hello World!\n";
-	std::cout << Sum(4, 5);
 
+	std::cout << addition(4, 5)  << "\n";
+	std::cout << subtraction(4, 5) << "\n";
+
+	int a = 5;
+	int b = 8;
+	duplicate(a, b);
+	std::cout << a << "\n";
+	std::cout << b << "\n";
+
+	// pointers
+	int firstvalue = 5, secondvalue = 15;
+	int* p1, * p2;
+
+	p1 = &firstvalue;  // p1 = address of firstvalue
+	p2 = &secondvalue; // p2 = address of secondvalue
+
+	*p1 = 10;          // value pointed to by p1 = 10
+	*p2 = *p1;         // value pointed to by p2 = value pointed to by p1
+
+	p1 = p2;           // p1 = p2 (value of pointer is copied)
+	*p1 = 20;          // value pointed to by p1 = 20
+
+	std::cout << "firstvalue is " << firstvalue << '\n';
+	std::cout << "secondvalue is " << secondvalue << '\n';
 }
