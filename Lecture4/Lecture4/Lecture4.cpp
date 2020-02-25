a9d7e240297fb93bb660e0a460a0692d5aae9eef
Jakub Plesnik <jakub.plesnik@sde.cz>
2020-02-25 17:42:09 +0100

    Add project files

--- Lecture4/Lecture4/Lecture4.cpp
+++ Lecture4/Lecture4/Lecture4.cpp
@@ -3,26 +3,7 @@
 
 int main()
 {
-    cout << "Lecture 3!\n";
+    std::cout << "Lecture 4 begins!\n";
 
-    Dictionary* dict = new Dictionary(10);
-    dict->AddKeyValue(new KeyValue(1, 2));
-    dict->AddKeyValue(new KeyValue(3, 6));
-    dict->AddKeyValue(new KeyValue(2, 4));
-    dict->AddKeyValue(new KeyValue(3, 8));
-
-    double result = dict->GetValueByIndex(1);
-
-    KeyValue* kv = dict->GetByKey(3);
-
-    delete dict;
-
-    Curve* c = new Curve(4);
-
-    c->AddPoint(new Point(0, 0));
-    c->AddPoint(new Point(0, 4));
-    c->AddPoint(new Point(4, 4));
-    c->AddPoint(new Point(4, 6));
-
-    double distance = c->GetTotalDistance();
+    User* user = new User("pepa@seznam.cz", "Admin123");
 }
