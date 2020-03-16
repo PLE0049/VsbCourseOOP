c7a8d6e4370d872da6e8bd00196d54de57956b05
Jakub Plesnik <jakub.plesnik@sde.cz>
2020-03-16 19:36:30 +0100

    Add Client implementation

---
+++ Lecture6/BankApplication/BankApplication/Client.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class Client
+{
+private: 
+	int code;
+	string name;
+public: 
+	Client(int c, string n);
+	int GetCode();
+	string GetName();
+};
+
